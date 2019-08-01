59512527099d38523132fe4bdf791f4449fdb972
liulei <liulei51@jd.com>
2019-08-01 13:51:49 +0800

    fix: 增加默认模板

---
+++ template/default/index.js
@@ -0,0 +1,11 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+Vue.use(Vuex)
+const state = {
+
+}
+const store = Vuex.store({
+  state
+})
+
+export default store
---
+++ template/default/postcss.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  plugins: {
+    autoprefixer: {},
+    'postcss-pxtorem': {
+      rootValue: 20,
+      unitPrecision: 5,
+      propList: [
+        '*'
+      ],
+      selectorBlackList: [],
+      replace: true,
+      mediaQuery: false,
+      minPixelValue: 2
+    }
+  }
+}
